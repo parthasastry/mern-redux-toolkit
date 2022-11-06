6f7fee5c69bae1d64e8a52f2171289141f07faa6
“Parthasarathy <pssastry@amazon.com>
2022-11-06 11:45:33 +0530

    server apis

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,29 @@
+const asyncHandler = require('express-async-handler')
+
+const getGoals = asyncHandler (async (req, res) => {
+    res.status(200).json({message: "Get Goals"})
+})
+
+const setGoal = asyncHandler (async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field in the body');
+    }
+
+    res.status(200).json({message: "Set Goal"})
+})
+
+const updateGoal = asyncHandler (async (req, res) => {
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
+})
+
+const deleteGoal = asyncHandler (async (req, res) => {
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+})
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
